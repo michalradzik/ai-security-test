06f2279dab6e6e8430ddf93da6df85a59d8a17d0
Michal Radzik <leszekwita@o2.pl>
2026-08-23 11:51:19 +0200

    initial commit

---
+++ VulnerableController.java
@@ -0,0 +1,21 @@
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class VulnerableController {
+
+    // LUKA 1: SQL Injection
+    @GetMapping("/user")
+    public String getUser(@RequestParam String id) {
+        String sql = "SELECT * FROM users WHERE id = " + id;
+        return sql;
+    }
+
+    // LUKA 2: Hardkodowany sekret
+    private static final String API_KEY = "sk-1234567890abcdef";
+
+    // LUKA 3: Brak autoryzacji
+    @DeleteMapping("/admin/users/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        // Brak @PreAuthorize!
+    }
+}
